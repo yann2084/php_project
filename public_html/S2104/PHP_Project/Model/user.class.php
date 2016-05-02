88786f2997cc602eb1f3d51f323bc5738de0cb2c
VERNETTE Yann <vernetty@hostname.iut2.upmf-grenoble.fr>
2016-05-02 18:20:05 +0200

    premier ajout

---
+++ public_html/S2104/PHP_Project/Model/user.class.php
@@ -0,0 +1,12 @@
+<?php
+
+    // Un article en vente
+    class Article {
+        public $ref;       // Référence unique
+        public $libelle;   // Nom de l'article
+        public $categorie; // identifiant de catégorie
+        public $prix;      // le prix
+        public $image;     // Nom du fichier image
+    }
+
+?>
